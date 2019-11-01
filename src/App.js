941a0dc6e93fcb2258d6adb3505494288bf4b39f
Matt Moran <mattmoran50@gmail.com>
2019-11-01 13:09:20 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header-container centered">
+        <div className="sun centered">
+          <span className="logo">dwellings</span>
+        </div>
+      </header>
+      {/*  <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      */}
+    </div>
+  );
+}
+
+export default App;
