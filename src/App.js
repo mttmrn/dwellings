499e4461d70d646725737617e4dde6eec9447686
Matt Moran <mattmoran50@gmail.com>
2019-11-02 09:56:32 -0400

    Initial landing page build complete

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
 import React from 'react';
 import './App.css';
+import Cloud from './img/cloud.svg'
+import Home from './img/home.svg'
+import Bezier from './img/bezier.svg'
 
 function App() {
   return (
-    <div className="app">
-      <header className="header-container centered">
+    <div className="app relative">
+      <img src={Bezier} className="bezier absolute" alt="bezier background accent" />
+      <header className="header-container centered relative">
+        <img src={Cloud} className="absolute cloud-large left1" alt="cloud" />
+        <img src={Cloud} className="absolute cloud-med left2" alt="cloud" />
+        <img src={Cloud} className="absolute cloud-small left3" alt="cloud" />
+        <img src={Cloud} className="absolute cloud-small right3" alt="cloud" />
+        <img src={Cloud} className="absolute cloud-med right2" alt="cloud" />
+        <img src={Cloud} className="absolute cloud-large right1" alt="cloud" />
         <div className="sun centered">
           <span className="logo">dwellings</span>
         </div>
       </header>
-      {/*  <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      */}
+      <main className="flex">
+        <div className="container-left">
+          <h1 className="headline">Housing for Entrepreneurs</h1>
+          <h2 className="subhead">Become roommates with brilliant people in luxury homes across the globe.</h2>
+          <div className="signup-container centered space-between">
+            <input type="email" placeholder="Email Address" className="email-input regular" />
+            <button className="btn regular relative">
+              <div className="btn-inside absolute centered regular">Request an Invite</div></button>
+          </div>
+          <div className="flex space-between">
+            <div className="card centered"><p className="description-text">Know your roommates before making any commitments.</p></div>
+            <div className="card centered"><p className="description-text">Average rent is less than $900 per person. </p></div>
+            <div className="card centered"><p className="description-text">Long term and short term stays available.</p></div>
+          </div>
+        </div>
+        <div className="centered container-right">
+          <img className="home-img" src={Home} alt="home with people inside" />
+        </div>
+      </main>
+
+
     </div>
   );
 }
