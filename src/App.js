cc1f596bace5d6845af0754600c5ca1a264c4ecf
Matt Moran <mattmoran50@gmail.com>
2019-11-05 21:47:46 -0500

    changed image pathfiles and added mailchimp

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import './App.css';
 import { Image } from 'cloudinary-react';
-import Bezier from './img/bezier.svg';
+
+
+
+
+
 
 function App() {
 
   return (
     <div className="app relative">
-      <Image cloudName="m2h" publicId="bezierBackgroundAccent.svg" className="bezier absolute" alt="background accent" />
+      <Image cloudName="m2h" publicId="/dwellings/bezierBackgroundAccent.svg" className="bezier absolute" alt="background accent" />
       <header className="header-container centered relative">
-        <Image cloudName="m2h" publicId="cloud.svg" className="absolute cloud-large left1" alt="cloud" />
-        <Image cloudName="m2h" publicId="cloud.svg" className="absolute cloud-med left2" alt="cloud" />
-        <Image cloudName="m2h" publicId="cloud.svg" className="absolute cloud-small left3" alt="cloud" />
-        <Image cloudName="m2h" publicId="cloud.svg" className="absolute cloud-small right3" alt="cloud" />
-        <Image cloudName="m2h" publicId="cloud.svg" className="absolute cloud-med right2" alt="cloud" />
-        <Image cloudName="m2h" publicId="cloud.svg" className="absolute cloud-large right1" alt="cloud" />
+        <Image cloudName="m2h" publicId="/dwellings/cloud.svg" className="absolute cloud-large left1" alt="cloud" />
+        <Image cloudName="m2h" publicId="/dwellings/cloud.svg" className="absolute cloud-med left2" alt="cloud" />
+        <Image cloudName="m2h" publicId="/dwellings/cloud.svg" className="absolute cloud-small left3" alt="cloud" />
+        <Image cloudName="m2h" publicId="/dwellings/cloud.svg" className="absolute cloud-small right3" alt="cloud" />
+        <Image cloudName="m2h" publicId="/dwellings/cloud.svg" className="absolute cloud-med right2" alt="cloud" />
+        <Image cloudName="m2h" publicId="/dwellings/cloud.svg" className="absolute cloud-large right1" alt="cloud" />
         <div className="sun centered">
           <span className="logo">dwellings</span>
         </div>
@@ -24,9 +28,11 @@ function App() {
           <h1 className="headline">Housing for Entrepreneurs</h1>
           <h2 className="subhead">Become roommates with brilliant people in luxury homes across the globe.</h2>
           <div className="signup-container centered space-between">
-            <input type="email" placeholder="Email Address" className="email-input regular" />
-            <button className="btn regular relative">
-              <div className="btn-inside absolute centered regular">Request an Invite</div></button>
+            <form className="form centered space-between" action="https://dwellings.us4.list-manage.com/subscribe/post?u=76a0a0db386b0988f945a84cf&amp;id=58fc527afa" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" novalidate>
+              <input type="email" name="EMAIL" id="mce-EMAIL" placeholder="Email Address" className="email-input regular" />
+              <button className="btn regular relative">
+                <div className="btn-inside absolute centered regular">Request an Invite</div></button>
+            </form>
           </div>
           <div className="flex space-between">
             <div className="card centered"><p className="description-text">Know your roommates before making any commitments.</p></div>
@@ -35,7 +41,7 @@ function App() {
           </div>
         </div>
         <div className="centered container-right">
-          <Image cloudName="m2h" publicId="home.svg" className="home-img" alt="home with people inside" />
+          <Image cloudName="m2h" publicId="/dwellings/home.svg" className="home-img" alt="home with people inside" />
         </div>
       </main>
 
