7d58dfaa36829eb1463f615563e1e34947a3b2bf
Matt Moran <mattmoran50@gmail.com>
2019-11-07 19:35:57 -0500

    Adding responsiveness

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import './App.css';
 import { Image } from 'cloudinary-react';
 
 
-
-
-
-
 function App() {
 
   return (
@@ -29,14 +25,14 @@ function App() {
           <h2 className="subhead">Become roommates with brilliant people in luxury homes across the globe.</h2>
           <div className="signup-container centered space-between">
             <form className="form centered space-between" action="https://dwellings.us4.list-manage.com/subscribe/post?u=76a0a0db386b0988f945a84cf&amp;id=58fc527afa" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" novalidate>
-              <input type="email" name="EMAIL" id="mce-EMAIL" placeholder="Email Address" className="email-input regular" />
+              <input type="email" name="EMAIL" id="mce-EMAIL" placeholder="Email Address" className="email-input regular" required />
               <button className="btn regular relative">
                 <div className="btn-inside absolute centered regular">Request an Invite</div></button>
             </form>
           </div>
           <div className="flex space-between">
             <div className="card centered"><p className="description-text">Know your roommates before making any commitments.</p></div>
-            <div className="card centered"><p className="description-text">Average rent is less than $900 per person. </p></div>
+            <div className="card centered"><p className="description-text">Average rent is less than $900 per person.</p></div>
             <div className="card centered"><p className="description-text">Long term and short term stays available.</p></div>
           </div>
         </div>
